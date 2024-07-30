08b04e8f46a5be04b335b803a058639b513de3bd
Vakura100 <guillermo.ortiz@uniat.edu.mx>
2024-07-29 22:12:31 -0600

    caca seca chupada
    mmm rico

---
+++ ProyectoFisica/Assets/Scripts/Ganar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ganar : MonoBehaviour
+{
+    // Este método se llama cuando el jugador entra en el área del Trigger Collider
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Aquí puedes realizar acciones cuando el jugador entra en el área
+            // Por ejemplo, terminar el juego o mostrar un mensaje
+            Debug.Log("¡Jugador entró en el área!");
+        }
+    }
+}
