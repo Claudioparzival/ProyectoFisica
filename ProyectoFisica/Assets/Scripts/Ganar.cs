4a894583089bf85c5b3c8634124650b5046ef64e
Claudioparzival <150973451+Claudioparzival@users.noreply.github.com>
2024-07-30 14:57:26 -0600

    sexo con enanos
    que rico

--- ProyectoFisica/Assets/Scripts/Ganar.cs
+++ ProyectoFisica/Assets/Scripts/Ganar.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEditor;
 
 public class Ganar : MonoBehaviour
+
 {
+
+
+    [SerializeField] private GameObject Win;
     // Este método se llama cuando el jugador entra en el área del Trigger Collider
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -11,7 +18,13 @@ public class Ganar : MonoBehaviour
         {
             // Aquí puedes realizar acciones cuando el jugador entra en el área
             // Por ejemplo, terminar el juego o mostrar un mensaje
+            Victoria();
             Debug.Log("¡Jugador entró en el área!");
         }
+    }
+
+    public void Victoria()
+    {
+        Win.SetActive(true);
     }
 }
--- ProyectoFisica/Assets/Scripts/Muertecaja.cs
+++ ProyectoFisica/Assets/Scripts/Muertecaja.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Muertecaja : MonoBehaviour
 {
+
+
+    public Rigidbody2D rb2D;
+
+    public void Start()
+    {
+        
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.transform.gameObject.CompareTag("Player"))
